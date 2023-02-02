a59e94e4c5e8655e1143eab89853c3e66653d477
ANCollins <dre1825@gmail.com>
2023-02-01 19:47:28 -0500

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -14,5 +14,46 @@ langT = LanguageTranslatorV3(
     version='{version}',
     authenticator=authenticator
 )
+langT.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
-langT.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+"""
+Function to translate English to French
+"""
+def englishToFrench(englishText):
+
+    # This is to check for Null or empty text field.
+    if englishText == "":
+        return "Enter English Text"
+    
+    # We can proceed with the rest of the code if Null check passes.
+    else:
+        
+        # Translates the english text to french text.
+        translation = langT.translate(
+            text = englishText,
+            model_id = 'en-fr'
+            ).get_result()
+
+        frenchText = translation['translations'][0]['translation']
+        return frenchText
+
+"""
+Function to translate French to English
+"""
+def frenchToEnglish(frenchText):
+
+    # This is to check for Null or empty text field.
+    if frenchText == "":
+        return "Enter French Text"
+
+    else:
+
+        # Translates the english text to french text.
+        translation = langT.translate(
+            text = frenchText,
+            model_id = 'fr-en'
+            ).get_result()
+
+        englishText = translation['translations'][0]['translation']
+        return englishText
+
