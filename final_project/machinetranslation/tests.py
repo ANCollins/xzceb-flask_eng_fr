f76dd50d03222419d44f5755b81ce56bdcb04ee7
ANCollins <dre1825@gmail.com>
2023-02-02 20:00:07 -0500

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,44 +1,38 @@
-from translator import englishToFrench,frenchToEnglish
+"""Test cases for French to English translations."""
+from translator import english_to_french,french_to_english
 import unittest
 
-"""
-Test cases for English to French translations.
-"""
-class testEnglishToFrench(unittest.TestCase):
+# Test cases for English to French translations.
+class test_English_To_French(unittest.TestCase):
     # Andre Collins
-    def test_englishToFrench(self):
-        
+    def test_english_to_french(self):        
         # Checks for an empty field or null.
-        self.assertEqual(englishToFrench(''),'Empty')
+        self.assertEqual(english_to_french(''),'Empty')
         # Test for the translation of the word ‘Hello’ and ‘Bonjour’.
-        self.assertEqual(englishToFrench('Hello'),'Bonjour')
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
         # Test for the translation of the word ‘Good Day’ and ‘Bonne journée’.
-        self.assertEqual(englishToFrench('Good Day'),'Bonne journée')
+        self.assertEqual(english_to_french('Good Day'),'Bonne journée')
         # Test for the translation of the word ‘Good Afternoon’ and ‘Bon après-midi’.
-        self.assertEqual(englishToFrench('Good Afternoon'),'Bon après-midi')
+        self.assertEqual(english_to_french('Good Afternoon'),'Bon après-midi')
         # Test to see if the inputs are not equal
-        self.assertNotEqual(englishToFrench('Good Afternoon'),'Bon après-midi')
+        self.assertNotEqual(english_to_french('Good Afternoon'),'Bon après-midi')
         # Test to see if the inputs are not equal
-        self.assertNotEqual(englishToFrench('Good Afternoon'),'Good Afternoon')
+        self.assertNotEqual(english_to_french('Good Afternoon'),'Good Afternoon')
 
-"""
-Test cases for French to English translations.
-"""
-class testFrenchToEnglish(unittest.TestCase):
+# Test cases for French to English translations.
+class testFrench_To_English(unittest.TestCase):
     # Andre Collins
-    def test_frenchToEnglish(self):
-
+    def test_french_to_english(self):
         # Checks for an empty field or null.
-        self.assertEqual(frenchToEnglish(''),'Empty')
+        self.assertEqual(french_to_english(''),'Empty')
         # Test for the translation of the word ‘Bonjour’ and ‘Hello’.
-        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')        
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
         # Test for the translation of the word ‘Bonne journée’ and ‘Good Day’.
-        self.assertEqual(frenchToEnglish('Bonne journée'),'Good Day')
+        self.assertEqual(french_to_english('Bonne journée'),'Good Day')
         # Test for the translation of the word ‘Bon après-midi’ and ‘Good Afternoon’.
-        self.assertEqual(frenchToEnglish('Bon après-midi'),'Good Afternoon')
+        self.assertEqual(french_to_english('Bon après-midi'),'Good Afternoon')
         # Test to see if the inputs are not equal
-        self.assertNotEqual(frenchToEnglish('Bon après-midi'),'Good Afternoon')
+        self.assertNotEqual(french_to_english('Bon après-midi'),'Good Afternoon')
         # Test to see if the inputs are not equal
-        self.assertNotEqual(frenchToEnglish('Bon après-midi'),'Bon après-midi')
-
-unittest.main()
+        self.assertNotEqual(french_to_english('Bon après-midi'),'Bon après-midi')
+        unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,14 +1,13 @@
+"""Module providing Function translating English and French."""
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 apikey = os.environ['apikey']
 url = os.environ['url']
-
 authenticator = IAMAuthenticator('{apikey}')
 langT = LanguageTranslatorV3(
     version='{version}',
@@ -16,44 +15,28 @@ langT = LanguageTranslatorV3(
 )
 langT.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
-"""
-Function to translate English to French
-"""
-def englishToFrench(englishText):
-
+def english_to_french(english_text):
+    """ Function to translate English to French. """
     # This is to check for Null or empty text field.
-    if englishText == "":
+    if english_text == "":
         return "Enter English Text"
-    
-    # We can proceed with the rest of the code if Null check passes.
-    else:
-        
-        # Translates the english text to french text.
-        translation = langT.translate(
-            text = englishText,
-            model_id = 'en-fr'
-            ).get_result()
+    # Translates the english text to french text.
+    translation = langT.translate(
+        text = english_text,
+        model_id = 'en-fr'
+        ).get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-        frenchText = translation['translations'][0]['translation']
-        return frenchText
-
-"""
-Function to translate French to English
-"""
-def frenchToEnglish(frenchText):
-
+def french_to_english(french_text):
+    """ Function to translate French to English """
     # This is to check for Null or empty text field.
-    if frenchText == "":
+    if french_text == "":
         return "Enter French Text"
-
-    else:
-
         # Translates the english text to french text.
-        translation = langT.translate(
-            text = frenchText,
-            model_id = 'fr-en'
-            ).get_result()
-
-        englishText = translation['translations'][0]['translation']
-        return englishText
-
+    translation = langT.translate(
+        text = french_text,
+        model_id = 'fr-en'
+        ).get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
